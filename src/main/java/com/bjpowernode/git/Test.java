59c73879110621a24d29ceaeaab69456817cb8c4
wxx <wxx@163.com>
2021-03-21 15:23:36 +0800

    创建工程get-test

---
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.git;
+
+/**
+ * Create by csc on 2021/3/21.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+    }
+}
